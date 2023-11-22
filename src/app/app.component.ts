70b0b42fbeaac7532649ea9be31c99badb5e4650
Pericles <slouis482@gmail.com>
2023-11-22 00:58:33 -0400

    acttualizaciones  dinamimcas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { dateConvert } from './functions/date';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { MessageService } from 'primeng/api';
+import { filter, interval } from 'rxjs';
 
 
 @Component({
@@ -9,10 +12,51 @@ import { dateConvert } from './functions/date';
 })
 export class AppComponent implements OnInit {
   sessionDate: Date = new Date();
+  /**
+   *booleano que define si existen actualizaciones
+   *
+   * @type {boolean}
+   * @memberof AppComponent
+   */
+  updates: boolean = false
 
-  ngOnInit(): void {
-    localStorage.setItem('timeSession',dateConvert(this.sessionDate))
-    
+  constructor(
+    private readonly swUpdate: SwUpdate,
+    private messageService: MessageService,
+  ) {
+    setInterval(() => {
+      this.checkForUpdates()
+    }, 6000) //6s
   }
 
+
+
+  ngOnInit(): void {
+
+    localStorage.setItem('timeSession', dateConvert(this.sessionDate))
+
+  }
+  /**
+   *Busca actualizaciones
+   *
+   * @memberof AppComponent
+   */
+  checkForUpdates(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.checkForUpdate().then((event) => {
+        
+        if (!this.updates) {
+          this.updates = event
+        } else return
+      });
+    }
+  }
+  /**
+   *refresca la pagina 
+   *
+   * @memberof AppComponent
+   */
+  activateUpdate() {
+    window.location.href = window.location.href;
+  }
 }
