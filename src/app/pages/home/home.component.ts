d24163fdf4b5de203a09398b14d09c29bb0c6460
Pericles <slouis482@gmail.com>
2024-03-11 23:15:00 -0400

    Se esa creando el tutorial

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ConfirmationService, MessageService } from 'primeng/api';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MessageService } from 'primeng/api';
 import { Dialog } from 'primeng/dialog';
-import { OverlayPanel } from 'primeng/overlaypanel';
+import { ConversionCoinComponent } from 'src/app/components/conversion-coin/conversion-coin.component';
+import { StatusCoinComponent } from 'src/app/components/status-coin/status-coin.component';
 import { BankingRole } from 'src/app/enum/entiesBanking';
 import { Tutorial } from 'src/app/interfaces/tutorial';
 import { Bank } from 'src/app/models/bank';
@@ -14,8 +15,10 @@ import { AppConfigService } from 'src/app/services/app-config.service';
 })
 export class HomeComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('anchor_tutorial') anchor_tutorial: any;
   @ViewChild('dialog_tutorial') dialog_tutorial!: Dialog;
+  @ViewChild('status_coin') status_coin!: StatusCoinComponent;
+  @ViewChild('conversion_coin') conversion_coin !: ConversionCoinComponent;
+
 
 
   entidadBancaria!: BankingRole
@@ -30,11 +33,9 @@ export class HomeComponent implements OnInit, AfterViewInit {
     id: 0
   }
 
-  position_modal: { 'left.px': string, 'top.px': string, 'bottom.px': string, 'right.px': string } = {
-    'left.px': '100',
-    'top.px': '100',
-    'bottom.px': '100',
-    'right.px': '100'
+  position_modal: { 'left.px': string, 'top.px': string } = {
+    'left.px': '0',
+    'top.px': '0'
   }
 
   constructor(
@@ -50,11 +51,20 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
   ngAfterViewInit() {
     setTimeout(() => {
-      this.validateTutorial()
 
+      this.validateTutorial()
     }, 1000)
 
 
+  }
+  /**
+   *Data del tutorial
+   *
+   * @readonly
+   * @memberof HomeComponent
+   */
+  get getDataTutorial() {
+    return this.appConfigService.contend_tutorial[this.appConfigService.position_tutorial];
   }
 
   inactiveScrollBody() {
@@ -79,60 +89,55 @@ export class HomeComponent implements OnInit, AfterViewInit {
     if (!localStorage.getItem('passTutorial')) localStorage.setItem('passTutorial', '0')
 
     if (parseInt(localStorage.getItem('passTutorial') as string) == 0) {
-      this.appConfigService.tutorial = true
       this.runTutorial()
+      this.appConfigService.tutorial = true
 
-    } this.appConfigService.tutorial = false
+    } else this.appConfigService.tutorial = false
   }
 
-  /**
-   *Valida ta vairbale tutorial
-   *
-   * @readonly
-   * @memberof HomeComponent
-   */
-  get start_tutorial() {
-    return this.appConfigService.tutorial
-  }
+
   /**
    *Abre lo necesario del tutorial
    *
    * @memberof HomeComponent
    */
   runTutorial() {
-
-    let dataTutorial: Tutorial = this.appConfigService.contend_tutorial[this.appConfigService.position_tutorial];
-
-
-
-    this.openModalRunTutorial(dataTutorial, this.anchor_tutorial.elementRef)
+    let dataTutorial: Tutorial = this.getDataTutorial
+    let dataModal = { top: '0', left: '0' }
+    this.openModalRunTutorial(dataTutorial, dataModal)
 
   }
 
   /**
-   *Configura la posision del modal
+    ABRE Y CONFIGURA LA POSITION DE MODAL
    *
    * @param {Tutorial} dataTutorial
    * @memberof HomeComponent
    */
-  openModalRunTutorial(dataTutorial: Tutorial, element: ElementRef) {
+  openModalRunTutorial(dataTutorial: Tutorial, positionModal: { top: string, left: string }) {
 
-    const rect = element.nativeElement.getBoundingClientRect();
+
 
     this.itemContendTutorial = dataTutorial
 
-    this.position_modal['top.px'] = rect.top
-    this.position_modal['bottom.px'] = rect.bottom
-    this.position_modal['right.px'] = rect.right
-    this.position_modal['left.px'] = rect.left
-
-    //this.dialog_tutorial 
     this.viewModal = true
 
-    console.log(this.position_modal);
+    this.position_modal['top.px'] = positionModal.top;
+    // this.position_modal['bottom.px'] = rect.bottom
+    // this.position_modal['right.px'] = rect.right
+    this.position_modal['left.px'] = positionModal.left
 
+    //this.dialog_tutorial 
+    this.dialog_tutorial.style = this.position_modal
 
+  }
 
+  nextTutorial() {
+    this.appConfigService.position_tutorial++
+    this.itemContendTutorial = this.getDataTutorial
+    //section_tutorial_1
+    //*la idea es obtener dinamicamente los id de los divs
+    console.log(this.status_coin.elementRef.nativeElement);
 
   }
 
